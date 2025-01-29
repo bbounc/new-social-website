8110004c18cd075e9c0cbc5e55e9b2a26a783b7f
Brian Bordeaux <bbo@ad.unc.edu>
2025-01-29 13:09:47 -0500

    start of ui

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), require('daisyui')],
 	server: {
 		port: 3000,
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-				changeOrigin: true,
-			},
-		},
 	},
 });
