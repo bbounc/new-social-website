c04ce4ee6410b26c348c9ba40096f357c240fec6
Brian Bordeaux <bbo@ad.unc.edu>
2025-02-10 11:03:57 -0500

    deploy attempt 5

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -21,7 +21,7 @@ const SignUpPage = () => {
 
   const { mutate, isError, isPending, error } = useMutation({
     mutationFn: async (userData) => {
-      const res = await fetch("http://localhost:5002/api/auth/signup", {
+      const res = await fetch("/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(userData),
